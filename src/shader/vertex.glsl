9c52432420bc631af381018373b5f7ef1fa4a417
c2020love <connie2313113440@gmail.com>
2022-10-15 13:53:49 +0800

    first commit

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,13 +1,15 @@
 #version 330 core
     layout (location = 0) in vec3 aPos;
-    layout (location = 1) in vec3 aColor;
     layout (location = 2) in vec2 aTexCoord;
     out vec2 TexCoord;
-    out vec3 ourColor;
-    //out vec3 ourPosition;
+   
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 project;
+    uniform mat4 transform;
+
     void main()
     {
-      gl_Position = vec4(aPos,1.0f);
-       ourColor=aColor;
-       TexCoord = aTexCoord;
+      gl_Position = project*view*model*transform*vec4(aPos,1.0f);
+       TexCoord = vec2(aTexCoord.x,aTexCoord.y);
     }
